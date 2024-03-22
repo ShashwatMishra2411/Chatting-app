ac47c9f5b481a19c011a2c087b706731109ba1b6
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-23 04:04:58 +0530

    ✨ Update socket.io configuration and message handling

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-const io = require('socket.io')(5000)
+const io = require('socket.io')(5000, {
+    cors: {
+      origin: '*',
+    }
+  });
+  
+  const connectedClients = {};
 
-io.on('connection', socket =>{
-    const id = socket.handshake.query.id
-    socket.join(id)
+  io.on('connection', socket => {
+    const id = socket.handshake.query.id;
+    socket.join(id);
+    connectedClients[id] = socket;
 
-    socket.on('send-message', ({recipients, text}) =>{
-        recipients.forEach(recipient=>{
-            const newRecipients = recipients.filter(r => r!== recipient)
-        })
-    })
-})
+    console.log("Client connected:", id);
+    socket.on('send-message', ({ recipients, text }) => {
+      console.log("recipients", recipients)
+      recipients.forEach(recipient => {
+        const newRecipients = recipients.filter(r => r !== recipient);
+        newRecipients.push(id);
+        socket.broadcast.to(recipient).emit('receive-message', { recipients: newRecipients, sender: id, text });
+        console.log(newRecipients, recipient);
+      });
+    });
+  });
+  
