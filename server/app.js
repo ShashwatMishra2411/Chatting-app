011cf826dda16fb5704fd962d11dc3b663e6795d
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-24 01:22:57 +0530

    🎨 Update package.json and remove server.js

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app); // Create HTTP server instance
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+  }
+});
+
+const connectedClients = {};
+
+io.on('connection', socket => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+  connectedClients[id] = socket;
+
+  console.log("Client connected:", id);
+  socket.on('send-message', ({ recipients, text }) => {
+    console.log("recipients", recipients)
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit('receive-message', { recipients: newRecipients, sender: id, text });
+      console.log(newRecipients, recipient);
+    });
+  });
+});
+
+httpServer.listen(5000, () => {
+  console.log("Express server started on port 5000");
+});
