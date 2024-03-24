5bd93ecb4178e2cef2d6a43fed997cf0cb38719b
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-24 11:44:00 +0530

    Add root route and update private key files

--- server/app.js
+++ server/app.js
@@ -10,6 +10,10 @@ const io = require('socket.io')(httpServer, {
 
 const connectedClients = {};
 
+app.get('/', (req, res) => {
+  res.send('Hello, world! This is the root route.');
+});
+
 io.on('connection', socket => {
   const id = socket.handshake.query.id;
   socket.join(id);
