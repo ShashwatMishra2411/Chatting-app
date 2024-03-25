d0af7ab71d3f5fbce30ff345584c3d43aec0d8f9
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-25 12:25:31 +0530

    Update PORT variable in app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http'); // Import the http module
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT; 
 
 const server = http.createServer(app); // Create HTTP server instance
 
