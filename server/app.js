ff829a5fffc7ba05adf837be0b017f89a746ac2f
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-25 12:14:53 +0530

    Add .env file and update app.js and package.json

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http'); // Import the http module
+const PORT = process.env.PORT || 5000; 
 
 const server = http.createServer(app); // Create HTTP server instance
 
@@ -33,6 +34,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("Express server started on port 5000 (HTTP)");
 });
